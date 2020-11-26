f4eb8818739ad178fb9015199344f52fa3d84d47
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-11-26 17:05:06 +0530

    Add files via upload

---
+++ .github/workflows/DevUtility/TempApp/secondfile.sh
@@ -0,0 +1,10 @@
+echo off
+echo Hello World from $AGENT_NAME.
+echo My ID is $AGENT_ID.
+echo AGENT_WORKFOLDER contents:
+echo $AGENT_WORKFOLDER
+echo AGENT_BUILDDIRECTORY contents:
+echo $AGENT_BUILDDIRECTORY
+echo BUILD_SOURCESDIRECTORY contents:
+echo $BUILD_SOURCESDIRECTORY
+echo Over and out.
