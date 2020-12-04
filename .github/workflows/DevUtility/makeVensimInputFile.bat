a8ff6ab07deab0dafd2a2a738dee9a22e0d6fddf
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-12-04 13:01:08 +0530

    Add files via upload

--- .github/workflows/DevUtility/makeVensimInputFile.bat
+++ .github/workflows/DevUtility/makeVensimInputFile.bat
@@ -5,4 +5,5 @@ set filePath=%filePath:"=%
 set workspacePath= %3
 (for %%a in (%filePath%) do (
    echo "FILE>MDL2VMF|%workspacePath%%%a" >> %sourceFilePath%
+   echo "FILE>MDL2XMILE|%workspacePath%%%a" >> %sourceFilePath%
 ))
